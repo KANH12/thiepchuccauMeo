492aff99c411de02551ad954cff9d9e5e353b725
KANH12 <133483525+KANH12@users.noreply.github.com>
2024-02-26 21:05:58 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Trangg';
+const giftUrl = 'https://www.youtube.com/watch?v=wGF7PswOENQ';
+const eventName = 'Gửi cho cậu Mèo';
+const titleCard = 'Thùy Trangg';
+const contentCard = 'Thật sự tớ rất vui khi được quen biết cậu đấy. Mặc dù tớ đã hi vọng có thể tìm hiểu cậu tiếp nhưng mà chắc hiện tại cũng hơi khó nhỉ, đôi khi gặp được người tốt như cậu tớ mới kiểu trưởng thành lên được chớ. Có những điều tớ muốn nói cậu lắm nhưng mà bây giờ chắc cũng không còn cơ hội nên là thôi chắc nên để dịp khác. Chúc cậu sau này hạnh phúc, vui vẻ và tỏa sáng giống như cậu ở trong mắt tớ nhé!!';
+const giftImage = 'nttt.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
